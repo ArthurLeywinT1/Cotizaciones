a9736cedb9f3951c7f5e6108ad3619090e9b6cdb
chased36 <sebastiancaballero318@aragon.unam.mx>
2026-05-17 01:00:33 -0600

    Comentario borrado

--- lib/screens/usuariospantallas/home_operario.dart
+++ lib/screens/usuariospantallas/home_operario.dart
@@ -127,7 +127,6 @@ class _HomeOperarioScreenState extends ConsumerState<HomeOperarioScreen> {
           ),
           const Divider(height: 30),
 
-          // ESPACIO PARA LA TABLA
           Expanded(
             child: TablaOperarioScreen(
               area: widget.area,
